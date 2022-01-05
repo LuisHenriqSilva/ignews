221c907140b708dd9d02a562eac3dd3864b894c1
lh-silva <luis.silva@compasso.com.br>
2022-01-04 22:56:44 -0300

    signIn and SignOut button with next-auth

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,32 @@
-import { FaGithub } from "react-icons/fa";
-import { FiX } from "react-icons/fi";
+import {FaGithub} from "react-icons/fa";
+import {FiX} from "react-icons/fi";
+import {useSession, signIn, signOut } from "next-auth/react";
 
 import styles from './styles.module.scss'
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const {data: session} = useSession()
 
-  return isUserLoggedIn ? (
+  console.log("TESTEEEEEEEEE", session);
+
+  return session ? (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signOut()}
     >
       <FaGithub color="var(--green-500)"/>
-        Luis Silva
+        {session.user.name}
       <FiX color="#737380" className={styles.closeIcon}/>
     </button>
   ) : (
     <button
       type="button"
       className={styles.signInButton}
+      onClick={() => signIn('github')}
     >
       <FaGithub color="var(--yellow-500)"/>
-      Sign in with GitHub
+      SignIn with GitHub
     </button>
   )
 }
