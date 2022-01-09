626077caa41b1f9fddd7dda161f7469d4c9db5b6
lh-silva <luis.silva@compasso.com.br>
2022-01-09 19:31:22 -0300

    initial function for subscribe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,5 @@
+import {useSession, signIn, signOut} from "next-auth/react";
+
 import styles from './styles.module.scss'
 
 interface  SubscribeButtonProps {
@@ -5,11 +7,22 @@ interface  SubscribeButtonProps {
 }
 
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
+  const {data: session} = useSession()
 
+  function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return
+    }
+
+
+
+  }
   return (
     <button
       type='button'
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
