f5c194b018d9dc7266b6993c9b3aa988d54c0ddc
lh-silva <luis.silva@compasso.com.br>
2022-01-09 21:52:55 -0300

    stripe duplicated avoid

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,6 @@
 import {useSession, signIn, signOut} from "next-auth/react";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 
 import styles from './styles.module.scss'
 
@@ -9,13 +11,21 @@ interface  SubscribeButtonProps {
 export function SubscribeButton({priceId}: SubscribeButtonProps) {
   const {data: session} = useSession()
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if(!session) {
       signIn('github')
       return
     }
 
+    try {
+      const response = await api.post('/subscribe')
+      const {sessionId} = response.data
+      const stripe = await getStripeJs()
 
+      stripe.redirectToCheckout({sessionId})
+    } catch (err) {
+      alert(err.message)
+    }
 
   }
   return (
